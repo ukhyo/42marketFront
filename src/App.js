06725cb07614e902d760ad865d17e0bdef68539e
Ukwon <dnrgjs33@naver.com>
2021-10-14 14:07:30 +0900

    합치기전

--- src/App.js
+++ src/App.js
@@ -26,54 +26,5 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 // test App Component
-//function App() {
-//	let [test, setTest] = useState([]);
-//	let [test2, setTest2] = useState([]);
-//	useEffect(() => {
-//		let arr = [
-//			{
-//				name: "hi",
-//				age: 10,
-//				text: "real?",
-//			},
-//			{
-//				name: "h2",
-//				age: 12,
-//				text: "change?",
-//			},
-//			{
-//				name: " 그렇구나",
-//				age: 19,
-//				text: "tt",
-//			},
-//		];
-//		setTest(arr);
-//	}, []);
 
-//	const btnClick = () => {
-//		let temp = test;
-//		temp[0].age += 2;
-//		setTest((test = temp));
-//	};
-
-//	const btnClick2 = () => {
-//		let temp = {
-//			name: "안녕하세요",
-//			age: 1,
-//			text: "반갑습니다",
-//		};
-//		setTest2([...test2, temp]);
-//	};
-
-//	console.log("%c in Function", "color: blue");
-//	return (
-//		<BrowserRouter>
-//			{console.log(`%c Case 1`, "color:red", test, typeof test)}
-//			{console.log(`%c Case 2`, "color:blue", test2, typeof test2)}
-//			<h1>ㅎㅇ</h1>
-//			<button onClick={btnClick}>클릭해봐</button>
-//			<button onClick={btnClick2}>클릭해봐2</button>
-//		</BrowserRouter>
-//	);
-//}
 export default App;
