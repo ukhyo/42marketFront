ac6cb936e05828d7f1abec2fe0c7bc633fc1d7bb
Ukwon <dnrgjs33@naver.com>
2021-10-28 17:10:53 +0900

    gogo

--- src/testApp.js
+++ src/testApp.js
@@ -3,73 +3,88 @@ import styled from "styled-components";
 import axios from "axios";
 
 
-
+function test() {
+	const fucnTest = async () => {
+		const response = await axios.get("http://localhost:3001/posts");
+		console.log(response.data, "띵");
+		return (response.data);
+	}
+	return (fucnTest())
+}
 
 const App = () => {
-	const [File, setFile] = useState([]);
-
-	const fileOnChange = async (e) => {
-		const files = e.target.files;
-		let fileArr = Array.from(files);
-		fileArr.forEach(data => {
-			console.log(data);
-		})
-		fileArr.forEach(data => {
-			setFile((file) => {
-				return [...file, data];
-			});
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		test().then(value => {
+			console.log(value);
+			setData(value);
 		});
-	}
-	const SubmitHandle = async () => {
-		let formData = new FormData();
-		const config = {
-				header: { "content-type": "multipart/form-data" },
-			};
-		File.forEach((data, index) => {
-			formData.append("file", data)
-		})
-		for (let values of formData.values()) {
-			console.log(values, "값");
-		}
-		 const Food = {
-				name: "피자",
-				price: 13500,
-			};
+	}, [])
 
-		var object = {};
-		formData.forEach((value, key) => {
-			if (!object.hasOwnProperty(key)) {
-				object[key] = value;
-				return;
-			}
-			if (!Array.isArray(object[key])) {
-				object[key] = [object[key]];
-			}
-			object[key].Push(value);
-		});
-		var json = JSON.stringify(object);
-		formData.append("stringFood", JSON.stringify(Food));
-		let check = {
-			item: "zxz",
-			price: 1,
-		}
-		let check2 = new FormData();
-		check2.append("file", "file1");
-		check2.append("file12", "이론");
-		console.log(formData.getAll('file'));
-		const res = await axios.post("http://localhost:4000/data", formData, config).then(res => {
-			console.log(res,"이거머임");
-		});
-		console.log(res);
-	}
+	//const [File, setFile] = useState([]);
 
-	const testArr = ["/img/a.jpeg", "/img/b.jpeg", "/img/c.jpeg", "/img/d.jpeg",];
-	const Test = async () => {
-		let Arr = File.map((data) => {
-			return data.name;
-		});
+	//const fileOnChange = async (e) => {
+	//	const files = e.target.files;
+	//	let fileArr = Array.from(files);
+	//	fileArr.forEach(data => {
+	//		console.log(data);
+	//	})
+	//	fileArr.forEach(data => {
+	//		setFile((file) => {
+	//			return [...file, data];
+	//		});
+	//	});
+	//}
+	//const SubmitHandle = async () => {
+	//	let formData = new FormData();
+	//	const config = {
+	//			header: { "content-type": "multipart/form-data" },
+	//		};
+	//	File.forEach((data, index) => {
+	//		formData.append("file", data)
+	//	})
+	//	for (let values of formData.values()) {
+	//		console.log(values, "값");
+	//	}
+	//	 const Food = {
+	//			name: "피자",
+	//			price: 13500,
+	//		};
 
-		console.log(Arr, "Arr 는?");
+	//	var object = {};
+	//	formData.forEach((value, key) => {
+	//		if (!object.hasOwnProperty(key)) {
+	//			object[key] = value;
+	//			return;
+	//		}
+	//		if (!Array.isArray(object[key])) {
+	//			object[key] = [object[key]];
+	//		}
+	//		object[key].Push(value);
+	//	});
+	//	var json = JSON.stringify(object);
+	//	formData.append("stringFood", JSON.stringify(Food));
+	//	let check = {
+	//		item: "zxz",
+	//		price: 1,
+	//	}
+	//	let check2 = new FormData();
+	//	check2.append("file", "file1");
+	//	check2.append("file12", "이론");
+	//	console.log(formData.getAll('file'));
+	//	const res = await axios.post("http://localhost:4000/data", formData, config).then(res => {
+	//		console.log(res,"이거머임");
+	//	});
+	//	console.log(res);
+	//}
+
+	//const testArr = ["/img/a.jpeg", "/img/b.jpeg", "/img/c.jpeg", "/img/d.jpeg",];
+	//const Test = async () => {
+	//	let Arr = File.map((data) => {
+	//		return data.name;
+	//	});
+
+	//	console.log(Arr, "Arr 는?");
 
 		//const Data = {
 		//	title: "제목",
@@ -79,20 +94,21 @@ const App = () => {
 		//	img: testArr,
 		//};
 		//await axios.post("http://localhost:4000/comments", Data);
-	}
+	//}
 
-
-
+	const hi = "hi";
+	console.log(hi);
+//hi
 	return (
 		<div>
 			<SC>
-
-				<label for="getFile">상품이미지</label>
+				{console.log(data, "data check")}
+				{/*<label for="getFile">상품이미지</label>
 				<input type="file" onChange={fileOnChange} multiple id="getFile" />
 				<button type="submit" onClick={SubmitHandle}>제출</button>
 
 				<div></div>
-				<button onClick={Test}>눌러봐유</button>
+				<button onClick={Test}>눌러봐유</button>*/}
 			</SC>
 		</div>
 	);
