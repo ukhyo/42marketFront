0016aa9befb97cfacf109857b39ef43412e4296d
Ukwon <dnrgjs33@naver.com>
2021-11-24 02:05:06 +0900

    합치기 성공?

--- src/Components/utils/Pagination.js
+++ src/Components/utils/Pagination.js
@@ -8,7 +8,7 @@ import { BsArrowRightShort } from "react-icons/bs";
 const Pagination = ({ postsPerPage, totalPosts, paginate, current }) => {
 	const ScrollToTop = () => {
 		window.scrollTo({
-			top: 100,
+			top: 0,
 			behavior: "smooth",
 		})
 	}
