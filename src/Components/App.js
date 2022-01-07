00329829b4fdb64843c4b7b69c4a54f9ecafa2d6
이형진 <hyeongjin3@hanmail.net>
2022-01-07 14:47:50 +0900

    build

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 import { setSocket } from '../modules/Socket';
 
-let socket = new SockJS("/4m2/");
+let socket = new SockJS("/4m2d/");
 let stompClient = Stomp.over(socket);
 stompClient.debug= () => {};
 
