378463601f7b0bf46fefceea429911f4fb4e59bd
이형진 <hyeongjin3@hanmail.net>
2022-01-12 01:04:48 +0900

    build

--- src/Components/Mainpage/Notification.js
+++ src/Components/Mainpage/Notification.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import SockJS from 'sockjs-client';
 import SockJsClient from 'react-stomp';
 import * as StompJs from '@stomp/stompjs';
+import Stomp from 'stompjs';
 import { setSocket } from '../../modules/Socket';
 
 function Notification() {
@@ -12,38 +13,16 @@ function Notification() {
 	const dispatch = useDispatch();
 
 	const onClick = () => {
-		///
-		const client = new StompJs.Client({
-			brokerURL: 'ws://www.4m2d.shop/4m2d',
-			connectHeaders: {
-			  login: 'user',
-			  passcode: 'password',
-			},
-			debug: function (str) {
-			  console.log(str);
-			},
-			reconnectDelay: 5000, //자동 재 연결
-			heartbeatIncoming: 4000,
-			heartbeatOutgoing: 4000,
-			});
-			client.onConnect = function (frame) {
-				console.log("connected");
-			};
-		client.onStompError = function (frame) {
-		  console.log('Broker reported error: ' + frame.headers['message']);
-		  console.log('Additional details: ' + frame.body);
-		};
-		client.activate();
-		//var socket = new SockJS('/4m2d', null, {transports: ["websocket", "xhr-streaming", "xhr-polling"]});
-		// let stompClient = Stomp.over(socket);
-		// stompClient.debug= () => {};
-		// dispatch(setSocket(socket, stompClient));
-		// stompClient.connect({}, ()=>{
-		// 	stompClient.subscribe(`/sub/all`, (data) => {
-		// 		console.log(data);
-		// 	})
-		// });
-		// console.log("connect try");
+		var socket = new SockJS('/api/ws');
+		let stompClient = Stomp.over(socket);
+		stompClient.debug= () => {};
+		dispatch(setSocket(socket, stompClient));
+		stompClient.connect({}, ()=>{
+			stompClient.subscribe(`/sub/all`, (data) => {
+				console.log(data);
+			})
+		});
+		console.log("connect try");
 		setIsActive(isActive);
 	}
 
