10191c4b6aff3c001c1d8e50845078337523a305
이형진 <hyeongjin3@hanmail.net>
2022-01-12 00:13:03 +0900

    build

--- src/Components/Mainpage/Notification.js
+++ src/Components/Mainpage/Notification.js
@@ -12,7 +12,7 @@ function Notification() {
 
 	const onClick = () => {
 		const client = new StompJs.Client({
-			brokerURL: 'ws://4m2d.shop/4m2d',
+			brokerURL: 'ws://www.4m2d.shop/4m2d',
 			connectHeaders: {
 			  login: 'user',
 			  passcode: 'password',
