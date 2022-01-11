f56411e04c5f82527f05ba7091dac66b7fb17580
이형진 <hyeongjin3@hanmail.net>
2022-01-12 00:49:28 +0900

    build

--- src/Components/Mainpage/Notification.js
+++ src/Components/Mainpage/Notification.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import styled from 'styled-components';
 import { useSelector, useDispatch } from "react-redux";
-import SockJsClient from 'react-stomp';
 import SockJS from 'sockjs-client';
+import SockJsClient from 'react-stomp';
 import * as StompJs from '@stomp/stompjs';
 import { setSocket } from '../../modules/Socket';
 
@@ -13,28 +13,27 @@ function Notification() {
 
 	const onClick = () => {
 		///
-		// const client = new StompJs.Client({
-		// 	brokerURL: 'ws://www.4m2d.s',
-		// 	connectHeaders: {
-		// 	  login: 'user',
-		// 	  passcode: 'password',
-		// 	},
-		// 	debug: function (str) {
-		// 	  console.log(str);
-		// 	},
-		// 	reconnectDelay: 5000, //자동 재 연결
-		// 	heartbeatIncoming: 4000,
-		// 	heartbeatOutgoing: 4000,
-		// 	});
-		// 	client.onConnect = function (frame) {
-		// 		console.log("connected");
-		// 	};
-		///
-		// client.onStompError = function (frame) {
-		//   console.log('Broker reported error: ' + frame.headers['message']);
-		//   console.log('Additional details: ' + frame.body);
-		// };
-		// client.activate();
+		const client = new StompJs.Client({
+			brokerURL: 'ws://www.4m2d.s',
+			connectHeaders: {
+			  login: 'user',
+			  passcode: 'password',
+			},
+			debug: function (str) {
+			  console.log(str);
+			},
+			reconnectDelay: 5000, //자동 재 연결
+			heartbeatIncoming: 4000,
+			heartbeatOutgoing: 4000,
+			});
+			client.onConnect = function (frame) {
+				console.log("connected");
+			};
+		client.onStompError = function (frame) {
+		  console.log('Broker reported error: ' + frame.headers['message']);
+		  console.log('Additional details: ' + frame.body);
+		};
+		client.activate();
 		//var socket = new SockJS('/4m2d', null, {transports: ["websocket", "xhr-streaming", "xhr-polling"]});
 		// let stompClient = Stomp.over(socket);
 		// stompClient.debug= () => {};
@@ -51,20 +50,10 @@ function Notification() {
 	return (
 		<div>
 			<NotiButtonC onClick={onClick}>알림</NotiButtonC>
-			{
-				isActive ? 
-				<SockJsClient url='http://4m2d.shop/4m2d' topics={['/sub/all']}
-				onMessage={(msg) => { console.log(msg); }}
-				ref={ (client) => { this.clientRef = client }} /> : 
-				console.log("isActive?")
-			}
-
-
 		</div>
 	)
 }
 
-const NotiButtonC = styled.button `
-`;
-
 export default Notification
+const NotiButtonC = styled.button `
+`
