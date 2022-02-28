94f952f72d3e5db28a68a66b68873085d708b852
ukheon <dnrgjs33@naver.com>
2022-03-01 00:18:59 +0900

    ukheon add remote

--- src/Components/Mainpage/Header.js
+++ src/Components/Mainpage/Header.js
@@ -1,274 +1,401 @@
 import React, { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+// import {AnimatePresence}
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
 import { useCookies, Cookies } from "react-cookie";
 import { useSelector, useDispatch } from "react-redux";
 import Notification from "./Notification";
-import { setSocket } from '../../modules/Socket';
+import { setSocket } from "../../modules/Socket";
 import { setUserId } from "../../modules/User";
 import { FiLogOut } from "react-icons/fi";
 import axios from "axios";
-
 function Header() {
-	const cookie = new Cookies()
-	let { userId: userId, Authorization: token, subscribes: sub } = cookie.getAll();
-	const [Loading, setLoading] = useState(true);
-	const history = useHistory();
-	const [text, setText] = useState("");
+  const cookie = new Cookies();
+  const [remoteState, setRemoteState] = useState(false);
+  const [idx, setIdx] = useState(0);
+  let {
+    userId: userId,
+    Authorization: token,
+    subscribes: sub,
+  } = cookie.getAll();
+  const [Loading, setLoading] = useState(true);
+  const history = useHistory();
+  const [text, setText] = useState("");
 
-	if (userId === undefined)
-		userId = "0";
-	// const onSetSocket = (socket, stompClient) => {
-	// 	dispatch(setSocket(socket, stompClient));
-	// };
-	function onChange(e) {
-		setText(e.target.value);
-	};
-	const check = (e) => {
-		if (e.key == "Enter") {
-			imgClick();
-		}
-	};
-	function imgClick(e) {
-		if (text === "")
-		{
-			alert("검색어를 입력해주세요!");
-			return ;
-		}
-		else {
-			history.push(
-			{
-				pathname: `/search/${userId}/${text}`,
-			});
-		}
-		setText("");
-	};
+  useEffect(() => {}, [remoteState]);
 
-	// function connectNoti() {
-	// 	stompClient.connect({}, ()=>{
-	// 		stompClient.subscribe(`/sub/all`, (data) => {
-	// 			console.log(data);
-	// 		})
-	// 	});
-	// }
-	const headers = {
-		"Authorization": `Bearer ${token}`,
-		"withCreadentials": true,
-		"Content-Type": "application/json",
-	};
+  if (userId === undefined) userId = "0";
+  // const onSetSocket = (socket, stompClient) => {
+  // 	dispatch(setSocket(socket, stompClient));
+  // };
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  const check = (e) => {
+    if (e.key == "Enter") {
+      imgClick();
+    }
+  };
+  function imgClick(e) {
+    if (text === "") {
+      alert("검색어를 입력해주세요!");
+      return;
+    } else {
+      history.push({
+        pathname: `/search/${userId}/${text}`,
+      });
+    }
+    setText("");
+  }
 
-	return (
-		<HeaderC>
-			<HeaderLineC>
-				<HeaderLogoC>
-					<a href="http://www.4m2d.site/" onClick={() => {
-						setLoading(!Loading);
-						window.scrollTo({
-							top: 0,
-						})
-					}}>
-						<HeaderLogoImgC src={process.env.PUBLIC_URL + "/img/Logo1.png"} />
-					</a>
-				</HeaderLogoC>
-				<HeaderSearchC>
-					<HeaderSearchInputC>
-						<img
-							onClick={imgClick}
-							className="header_search_img"
-							src={process.env.PUBLIC_URL + "/img/searchIcon.png"}
-							alt="img"
-						/>
-						<input
-							placeholder="검색어를 입력해주세요."
-							onChange={onChange}
-							onKeyDown={check}
-							type="text"
-							value={text}
-						/>
-					</HeaderSearchInputC>
-				</HeaderSearchC>
-				<HeaderInfoC>
-					{token ?
-						<LinkC to="/product/regi">
-							<img src={process.env.PUBLIC_URL + "/img/wonIcon.png"} />
-							<div>판매하기</div>
-						</LinkC>
-						:
-						null
-						// <AC onClick={() => {
-						// 	alert("로그인이 필요합니다.");
-						// }} href="https://api.intra.42.fr/oauth/authorize?client_id=2b02d6cbfa01cb92c9572fc7f3fbc94895fc108fc55768a7b3f47bc1fb014f01&redirect_uri=http%3A%2F%2Fapi.%2Flogin%2FgetToken&response_type=code">
-						// 	<img src={process.env.PUBLIC_URL + "/img/wonIcon.png"} />
-						// 	<div>판매하기</div>
-						// </AC>
-					}
-					{token ?
-					<LinkC to={`/mypage/${userId}/selllist`}>
-						<img src={process.env.PUBLIC_URL + "/img/userIcon.png"} />
-						<div>내정보</div>
-					</LinkC>
-						:
-						null
-						// <AC href="https://api.intra.42.fr/oauth/authorize?client_id=2b02d6cbfa01cb92c9572fc7f3fbc94895fc108fc55768a7b3f47bc1fb014f01&redirect_uri=http%3A%2F%2Fapi.4m2d.shop%2Flogin%2FgetToken&response_type=code" onClick={() => {
-						// 	alert("로그인이 필요합니다.")
-						// }}>
-						// 	<img src={process.env.PUBLIC_URL + "/img/userIcon.png"} />
-						// <div>내정보</div>
-					// </AC>
-					}
-					{/* {
+  // function connectNoti() {
+  // 	stompClient.connect({}, ()=>{
+  // 		stompClient.subscribe(`/sub/all`, (data) => {
+  // 			console.log(data);
+  // 		})
+  // 	});
+  // }
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    withCreadentials: true,
+    "Content-Type": "application/json",
+  };
+
+  const ScrollUp = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <HeaderC>
+      <HeaderLineC>
+        <HeaderLogoC>
+          <a
+            href="http://www.4m2d.site/"
+            onClick={() => {
+              setLoading(!Loading);
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
+          >
+            <HeaderLogoImgC src={process.env.PUBLIC_URL + "/img/Logo1.png"} />
+          </a>
+        </HeaderLogoC>
+        <HeaderSearchC>
+          <HeaderSearchInputC>
+            <img
+              onClick={imgClick}
+              className="header_search_img"
+              src={process.env.PUBLIC_URL + "/img/searchIcon.png"}
+              alt="img"
+            />
+            <input
+              placeholder="검색어를 입력해주세요."
+              onChange={onChange}
+              onKeyDown={check}
+              type="text"
+              value={text}
+            />
+          </HeaderSearchInputC>
+        </HeaderSearchC>
+        <HeaderInfoC>
+          {
+            token ? (
+              <LinkC to="/product/regi">
+                <img src={process.env.PUBLIC_URL + "/img/wonIcon.png"} />
+                <div>판매하기</div>
+              </LinkC>
+            ) : null
+            // <AC onClick={() => {
+            // 	alert("로그인이 필요합니다.");
+            // }} href="https://api.intra.42.fr/oauth/authorize?client_id=2b02d6cbfa01cb92c9572fc7f3fbc94895fc108fc55768a7b3f47bc1fb014f01&redirect_uri=http%3A%2F%2Fapi.%2Flogin%2FgetToken&response_type=code">
+            // 	<img src={process.env.PUBLIC_URL + "/img/wonIcon.png"} />
+            // 	<div>판매하기</div>
+            // </AC>
+          }
+          {
+            token ? (
+              <LinkC to={`/mypage/${userId}/selllist`}>
+                <img src={process.env.PUBLIC_URL + "/img/userIcon.png"} />
+                <div>내정보</div>
+              </LinkC>
+            ) : null
+            // <AC href="https://api.intra.42.fr/oauth/authorize?client_id=2b02d6cbfa01cb92c9572fc7f3fbc94895fc108fc55768a7b3f47bc1fb014f01&redirect_uri=http%3A%2F%2Fapi.4m2d.shop%2Flogin%2FgetToken&response_type=code" onClick={() => {
+            // 	alert("로그인이 필요합니다.")
+            // }}>
+            // 	<img src={process.env.PUBLIC_URL + "/img/userIcon.png"} />
+            // <div>내정보</div>
+            // </AC>
+          }
+          {/* {
 						// token ?
 						<Notification/>
 						//  : null
 					} */}
-					{token ?
-						<LinkC onClick={() => {
-							const LogOut = () => {
-								cookie.remove('Authorization', {
-									path: '/',
-									domain: '.4m2d.site',
-								});
-								cookie.remove('userId', {
-									path: '/',
-									domain: '.4m2d.site',
-								});
-								window.location.reload();
-							}
-							LogOut();
-					}} to={`/`}>
-						<img src={process.env.PUBLIC_URL + "/img/logoutIcon.png"} />
-						<div>로그아웃</div>
-					</LinkC>
-						:
-					<AC href="https://api.intra.42.fr/oauth/authorize?client_id=2b02d6cbfa01cb92c9572fc7f3fbc94895fc108fc55768a7b3f47bc1fb014f01&redirect_uri=http%3A%2F%2Fapi.4m2d.site%2Flogin%2FgetToken&response_type=code">
-						<img src={process.env.PUBLIC_URL + "/img/loginIcon.png"} />
-						<div>로그인</div>
-					</AC>
-					}
-				</HeaderInfoC>
-			</HeaderLineC>
-		</HeaderC>
-	);
+          {token ? (
+            <LinkC
+              onClick={() => {
+                const LogOut = () => {
+                  cookie.remove("Authorization", {
+                    path: "/",
+                    domain: ".4m2d.site",
+                  });
+                  cookie.remove("userId", {
+                    path: "/",
+                    domain: ".4m2d.site",
+                  });
+                  window.location.reload();
+                };
+                LogOut();
+              }}
+              to={`/`}
+            >
+              <img src={process.env.PUBLIC_URL + "/img/logoutIcon.png"} />
+              <div>로그아웃</div>
+            </LinkC>
+          ) : (
+            <AC href="https://api.intra.42.fr/oauth/authorize?client_id=2b02d6cbfa01cb92c9572fc7f3fbc94895fc108fc55768a7b3f47bc1fb014f01&redirect_uri=http%3A%2F%2Fapi.4m2d.site%2Flogin%2FgetToken&response_type=code">
+              <img src={process.env.PUBLIC_URL + "/img/loginIcon.png"} />
+              <div>로그인</div>
+            </AC>
+          )}
+        </HeaderInfoC>
+        <AnimatePresence>
+          {remoteState && (
+            <>
+              <RemoteOpt onClick={() => setRemoteState(!remoteState)}>
+                X
+              </RemoteOpt>
+              <RemotePart
+                key="box1"
+                initial={{ opacity: 0, y: 5 }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: 0.2,
+                    duration: 0.5,
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  y: 10,
+                  transition: {
+                    delay: 0.4,
+                    duration: 0.5,
+                  },
+                }}
+                size={35}
+              >
+                문의?
+              </RemotePart>
+              <RemotePart
+                key="box2"
+                initial={{ opacity: 0, y: 5 }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: 0.4,
+                    duration: 0.5,
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  y: 10,
+                  transition: {
+                    delay: 0.2,
+                    duration: 1,
+                  },
+                }}
+                size={27.5}
+              >
+                음
+              </RemotePart>
+            </>
+          )}
+        </AnimatePresence>
+        <AnimatePresence exitBeforeEnter>
+          {!remoteState ? (
+            <RemoteOpt
+              onClick={() => {
+                setRemoteState(!remoteState);
+                setIdx((prev) => prev + 1);
+              }}
+            >
+              클릭
+            </RemoteOpt>
+          ) : null}
+        </AnimatePresence>
+        <RemoteUp onClick={ScrollUp}>⬆</RemoteUp>
+      </HeaderLineC>
+    </HeaderC>
+  );
 }
 
 // Header style
 const HeaderC = styled.header`
-	width: 100%;
-	min-width: 1200px;
-	position: sticky;
-	top: 0;
-	z-index: 10;
-	display: flex;
-	justify-content: center;
-	background-color: #fdfdfd;
-	border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  width: 100%;
+  min-width: 1200px;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  background-color: #fdfdfd;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
 const HeaderLineC = styled.div`
-	position: relative;
-	width: 1200px;
-	height: 100px;
-	display: flex;
-	/*justify-content: space-between;*/
-	align-items: center;
+  position: relative;
+  width: 1200px;
+  height: 100px;
+  display: flex;
+  /*justify-content: space-between;*/
+  align-items: center;
+`;
+
+const RemotePart = styled(motion.div)`
+  position: fixed;
+  right: 5vw;
+  top: ${(props) => props.size}vh;
+  width: 3vw;
+  height: 3vw;
+  border-radius: 1.5vw;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+const St = motion;
+
+const RemoteOpt = styled(motion.div)`
+  position: fixed;
+  right: 5vw;
+  top: 42.5vh;
+  width: 3vw;
+  height: 3vw;
+  border-radius: 1.5vw;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+const RemoteUp = styled.div`
+  position: fixed;
+  right: 5vw;
+  top: 50vh;
+  width: 3vw;
+  height: 3vw;
+  border-radius: 1.5vw;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
 `;
 
 const HeaderLogoC = styled.div`
-	width: 180px;
+  width: 180px;
 `;
 
 const HeaderLogoImgC = styled.img`
-	width: 100%;
-	height: 80%;
+  width: 100%;
+  height: 80%;
 `;
 
 const HeaderSearchC = styled.div`
-	margin-left: 30px;
-	width: 650px;
-	padding-left: 25px;
-	padding-right: 25px;
-	height: 0;
+  margin-left: 30px;
+  width: 650px;
+  padding-left: 25px;
+  padding-right: 25px;
+  height: 0;
 `;
 
 const HeaderSearchInputC = styled.fieldset`
-	display: flex;
-	justify-content: space-between;
-	margin: 0;
-	padding: 0;
-	width: 100%;
-	border-bottom: 1.5px solid rgba(0,0,0, 0.7);
-	background-color: #fdfdfd;
-	& > input::placeholder {
-		color: rgba(0,0,0,0.2);
-	}
-	& > input {
-		background-color: #fdfdfd;
-		width: 100%;
-		font-size: 18px;
-		border: none;
-		outline: none;
-	}
-	& > input:hover {
-		width: 100%;
-		outline: none;
-	}
-	& a {
-		padding: 5px;
-	}
-	& img {
-		/*border:none;
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  border-bottom: 1.5px solid rgba(0, 0, 0, 0.7);
+  background-color: #fdfdfd;
+  & > input::placeholder {
+    color: rgba(0, 0, 0, 0.2);
+  }
+  & > input {
+    background-color: #fdfdfd;
+    width: 100%;
+    font-size: 18px;
+    border: none;
+    outline: none;
+  }
+  & > input:hover {
+    width: 100%;
+    outline: none;
+  }
+  & a {
+    padding: 5px;
+  }
+  & img {
+    /*border:none;
 		outline:none;*/
-		margin-right: 8px;
-		width: 20px;
-		height: 20px;
-	}
+    margin-right: 8px;
+    width: 20px;
+    height: 20px;
+  }
 `;
 
 const AC = styled.a`
-	display: flex;
-	align-items: center;
-	text-decoration: none;
-	color: black;
-	> div:hover {
-		border-bottom: 2px solid rgb(100,130, 238);
-	}
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+  > div:hover {
+    border-bottom: 2px solid rgb(100, 130, 238);
+  }
 `;
 
 const LinkC = styled(Link)`
-	display: flex;
-	align-items: center;
-	text-decoration: none;
-	color: black;
-	> div:hover {
-		border-bottom: 2px solid rgb(100,130, 238);
-	}
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+  > div:hover {
+    border-bottom: 2px solid rgb(100, 130, 238);
+  }
 `;
 
 const HeaderInfoC = styled.div`
-	position: relative;
-	top: 15px;
-	right:0;
-	display: flex;
-	justify-content: right;
-	width: 350px;
-	font-size: 16px;
-	& ${LinkC}, & ${AC} {
-		padding: 0 15px;
-	}
-	& ${LinkC}:last-child, & ${AC}:last-child {
-		padding-right: 0;
-	}
-	& > ${LinkC}:not(${LinkC}:first-child) {
-		border-left: 1px solid rgb(0, 0, 0, 0.1);
-	}
-	& > ${AC}:not(${AC}:first-child) {
-		border-left: 1px solid rgb(0, 0, 0, 0.1);
-	}
-	& div {
-		margin-left: 10px;
-	}
+  position: relative;
+  top: 15px;
+  right: 0;
+  display: flex;
+  justify-content: right;
+  width: 350px;
+  font-size: 16px;
+  & ${LinkC}, & ${AC} {
+    padding: 0 15px;
+  }
+  & ${LinkC}:last-child, & ${AC}:last-child {
+    padding-right: 0;
+  }
+  & > ${LinkC}:not(${LinkC}:first-child) {
+    border-left: 1px solid rgb(0, 0, 0, 0.1);
+  }
+  & > ${AC}:not(${AC}:first-child) {
+    border-left: 1px solid rgb(0, 0, 0, 0.1);
+  }
+  & div {
+    margin-left: 10px;
+  }
 `;
-
 
 export default Header;
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -17,33 +17,33 @@ import M_Category from "../M_Components/M_Category/M_Category";
 import M_Search from "../M_Components/M_Search/M_Search";
 import M_PostDetail from "../M_Components/M_PostManage/M_PostDetail";
 import M_ProductEdit from "../M_Components/M_Product/M_ProductEdit";
+
 function Router() {
-	const mobileWidth = useMediaQuery(
-		{maxWidth: 519}
-	)
-	return (
+  const mobileWidth = useMediaQuery({ maxWidth: 519 });
+  return (
     <div>
-			{mobileWidth ?
-				<Switch>
-					<Route path={"/"} exact component={M_Mainpage}></Route>
-					<Route path={"/product/:tabs"} exact component={M_Product}></Route>
-					<Route path={"/category/:id"} exact component={M_Category} />
-					<Route path={"/mypage/:id/:url"} component={M_Mypage}></Route>
-					<Route path={"/product/edit"} exact component={M_ProductEdit} />
-					<Route path={"/postview/:id"} exact component={M_PostDetail}></Route>
-					<Route path={"/search/:id/:word"}  exact component={M_Search} />
-				</Switch>
-					: <Switch>
-						<Route path={"/"} exact component={Mainpage}></Route>
-						<Route path={"/intro"} exact component={Intro}></Route>
-						<Route path={"/mypage/:id/:url"} component={Mypage}></Route>
-						<Route path={"/postview/:id"} exact component={PostDetail}></Route>
-						<Route path={"/product/edit"} exact component={ProductEdit} />
-						<Route path={"/product/:tabs"} exact component={Product}></Route>
-						<Route path={"/category/:id"} exact component={Category} />
-						<Route path={"/search/:id/:word"}  exact component={Search} />
-					</Switch>
-		}
+      {mobileWidth ? (
+        <Switch>
+          <Route path={"/"} exact component={M_Mainpage}></Route>
+          <Route path={"/product/:tabs"} exact component={M_Product}></Route>
+          <Route path={"/category/:id"} exact component={M_Category} />
+          <Route path={"/mypage/:id/:url"} component={M_Mypage}></Route>
+          <Route path={"/product/edit"} exact component={M_ProductEdit} />
+          <Route path={"/postview/:id"} exact component={M_PostDetail}></Route>
+          <Route path={"/search/:id/:word"} exact component={M_Search} />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route path={"/"} exact component={Mainpage}></Route>
+          <Route path={"/intro"} exact component={Intro}></Route>
+          <Route path={"/mypage/:id/:url"} component={Mypage}></Route>
+          <Route path={"/postview/:id"} exact component={PostDetail}></Route>
+          <Route path={"/product/edit"} exact component={ProductEdit} />
+          <Route path={"/product/:tabs"} exact component={Product}></Route>
+          <Route path={"/category/:id"} exact component={Category} />
+          <Route path={"/search/:id/:word"} exact component={Search} />
+        </Switch>
+      )}
     </div>
   );
 }
