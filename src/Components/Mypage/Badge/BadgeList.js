2c83982dfa8a7364ac151cd3c4caaee525c5f08d
이형진 <hyeongjin3@hanmail.net>
2022-03-10 17:12:22 +0900

    build

--- src/Components/Mypage/Badge/BadgeList.js
+++ src/Components/Mypage/Badge/BadgeList.js
@@ -22,7 +22,11 @@ function BadgeList() {
 const BadgeListC = styled.div`
 	width: 200px;
 	height: 500px;
+	padding: 10px;
+	
+	position: absolute;
 	background-color: green;
+
 	border: 1px solid rgba(0, 0, 0, 0.2);
 	> div {
 		width: 40px;
