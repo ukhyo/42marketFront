ef7861c1167765a0db0937163d5f74354e2654ac
이형진 <hyeongjin3@hanmail.net>
2022-04-03 22:11:08 +0900

    test

--- src/Components/Mypage/Badge/Badge.js
+++ src/Components/Mypage/Badge/Badge.js
@@ -48,11 +48,11 @@ const	BadgesC = styled.div`
 	display: flex;
 	justify-content: start;
 	position: relative;
+	border-radius: 1px solid rgba(0, 0, 0, 0.05);
 	margin-bottom: 15px;
 	> img {
 		margin-right: 5px;
-		width: 30px;
-		height: 30px;
+		width: 40px;
 		border-radius: 20px;
 	}
 `
